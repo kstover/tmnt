0aba49be0f09e67c5a2faa31f0a0c610533808a7
Kevin Stover <kstover@gmail.com>
2015-12-08 22:38:43 -0500

    Adding base html.

--- tmnt-tutorial.php
+++ tmnt-tutorial.php
@@ -8,30 +8,75 @@ Author: Kevin Stover
 License: GPLv2
 */
 
-// Quickest way to add an admin page.
+// Quick way to add an admin page.
 function tmnt_tutorial_add_menu(){
-	$page = add_menu_page( 'TMNT' , 'Pick a Turtle!', 'manage_options', 'tmnt-tutorial', 'tmnt_tutorial', 'dashicons-art' );
+	$page = add_menu_page( 'TMNT' , 'Pick a Character!', 'manage_options', 'tmnt-tutorial', 'tmnt_tutorial', 'dashicons-art' );
+	add_action( 'admin_print_styles-' . $page, 'tmnt_tutorial_js' );
 }
 
 add_action( 'admin_menu', 'tmnt_tutorial_add_menu' );
 
 function tmnt_tutorial() {
 	?>
-	Please select your favourite Ninja Turtle!
+	Please select your favourite TMNT Character!
 	<ul>
 		<li>
-			<label><input type="radio" name="turtle" value="leonardo"> Leonardo</label>
+			<label><input type="radio" name="char" value="leonardo"> Leonardo</label>
 		</li>
 		<li>
-			<label><input type="radio" name="turtle" value="raphael"> Raphael</label>
+			<label><input type="radio" name="char" value="raphael"> Raphael</label>
 		</li>
 		<li>
-			<label><input type="radio" name="turtle" value="michelangelo"> Michelangelo</label>
+			<label><input type="radio" name="char" value="michelangelo"> Michelangelo</label>
 		</li>
 		<li>
-			<label><input type="radio" name="turtle" value="donatello"> Donatello</label>
+			<label><input type="radio" name="char" value="donatello"> Donatello</label>
 		</li>
 	</ul>
-	Your favourite turtle is: <span class="fav-turtle"></span>
+<style>
+
+.contact-card {
+   border: 1px solid #ccc;
+   width: 400px;
+}
+.contact-card::after {
+   clear: both;
+   content: "";
+   display: block;
+}
+.contact-card .profile-pic {
+   float: left;
+   padding: 20px;
+   width: 100px;
+}
+.contact-card .profile-info {
+   float: left;
+   padding: 0 20px;
+   width: 205px;
+}
+
+
+</style>
+
+
+<div class="contact-card">
+   <div class="profile-pic">
+       <img src="http://placehold.it/100x100">
+   </div>
+   <div class="profile-info">
+       <h2>Raphael</h2>
+       <ul>
+           <li>Favourite Weapon: Sai</li>
+           <li>Favourite Colour: Red</li>
+           <li>Favourite Saying: "All right! An all you can BEAT buffet!"</li>
+           <li>Favourite Food: Pizza</li>
+       </ul>
+   </div>
+
+</div>
 	<?php
+}
+
+function tmnt_tutorial_js() {
+	
 }
