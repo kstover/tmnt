7d7fbcb4433118452d51dd8f99a1eab1bd3e1ffb
Kevin Stover <kstover@gmail.com>
2015-12-08 23:18:04 -0500

    Adding base JS file.

---
+++ admin.js
@@ -0,0 +1,5 @@
+jQuery( document ).ready( function( $ ) {
+	/**
+	 * JS code will go here!
+	 */
+} );
--- tmnt-tutorial.php
+++ tmnt-tutorial.php
@@ -11,72 +11,75 @@ License: GPLv2
 // Quick way to add an admin page.
 function tmnt_tutorial_add_menu(){
 	$page = add_menu_page( 'TMNT' , 'Pick a Character!', 'manage_options', 'tmnt-tutorial', 'tmnt_tutorial', 'dashicons-art' );
-	add_action( 'admin_print_styles-' . $page, 'tmnt_tutorial_js' );
+	add_action( 'admin_print_styles-' . $page, 'tmnt_tutorial_css_js' );
 }
 
 add_action( 'admin_menu', 'tmnt_tutorial_add_menu' );
 
+/**
+ * This is our main admin output function.
+ * It includes an inline style section and requires a JS file.
+ * 
+ * @since  1.0
+ * @return void
+ */
 function tmnt_tutorial() {
 	?>
+	<style>
+	.contact-card {
+	   border: 1px solid #ccc;
+	   width: 400px;
+	}
+	.contact-card::after {
+	   clear: both;
+	   content: "";
+	   display: block;
+	}
+	.contact-card .profile-pic {
+	   float: left;
+	   padding: 20px;
+	   width: 100px;
+	}
+	.contact-card .profile-info {
+	   float: left;
+	   padding: 0 20px;
+	   width: 205px;
+	}
+	</style>
+
 	Please select your favourite TMNT Character!
 	<ul>
 		<li>
-			<label><input type="radio" name="char" value="leonardo"> Leonardo</label>
+			<label><input type="radio" class="radio-character" name="character" value="leonardo"> Leonardo</label>
 		</li>
 		<li>
-			<label><input type="radio" name="char" value="raphael"> Raphael</label>
+			<label><input type="radio" class="radio-character" name="character" value="raphael"> Raphael</label>
 		</li>
 		<li>
-			<label><input type="radio" name="char" value="michelangelo"> Michelangelo</label>
+			<label><input type="radio" class="radio-character" name="character" value="michelangelo"> Michelangelo</label>
 		</li>
 		<li>
-			<label><input type="radio" name="char" value="donatello"> Donatello</label>
+			<label><input type="radio" class="radio-character" name="character" value="donatello"> Donatello</label>
 		</li>
 	</ul>
-<style>
 
-.contact-card {
-   border: 1px solid #ccc;
-   width: 400px;
-}
-.contact-card::after {
-   clear: both;
-   content: "";
-   display: block;
-}
-.contact-card .profile-pic {
-   float: left;
-   padding: 20px;
-   width: 100px;
-}
-.contact-card .profile-info {
-   float: left;
-   padding: 0 20px;
-   width: 205px;
-}
-
-
-</style>
-
-
-<div class="contact-card">
-   <div class="profile-pic">
-       <img src="http://placehold.it/100x100">
-   </div>
-   <div class="profile-info">
-       <h2>Raphael</h2>
-       <ul>
-           <li>Favourite Weapon: Sai</li>
-           <li>Favourite Colour: Red</li>
-           <li>Favourite Saying: "All right! An all you can BEAT buffet!"</li>
-           <li>Favourite Food: Pizza</li>
-       </ul>
-   </div>
-
-</div>
+	<div class="contact-card">
+	   <div class="profile-pic">
+	       <img src="http://placehold.it/100x100">
+	   </div>
+	   <div class="profile-info">
+	       <h2 class="name"></h2>
+	       <ul>
+	           <li>Favourite Weapon: <span class="weapon"></span></li>
+	           <li>Favourite Colour: <span class="colour"></span></li>
+	           <li>Favourite Saying: <span class="saying"></span></li>
+	           <li>Favourite Food: <span class="food"></span></li>
+	       </ul>
+	   </div>
+	</div>
 	<?php
 }
 
-function tmnt_tutorial_js() {
-	
+function tmnt_tutorial_css_js() {
+	wp_enqueue_script( 'tmnt-tutorial', plugin_dir_url( __FILE__ ) .'admin.js' );
 }
